1415e8e6bcafa5eff5408ce3472614872ee33c4b
Alessio <alessiogdanz@gmail.com>
2020-03-24 18:05:48 -0500

    added comments on basicNumpy

--- basicNumpy.py
+++ basicNumpy.py
@@ -6,4 +6,27 @@ import numpy as np
 #   deactivate
 
 # Arrays
+#   unidimensional
 a = np.array([1,2,3])
+# a.dtype implicit and explicit
+# a.shape
+#   multisimensional
+a2 = np.array([[1,2,3],[4,5,6]])
+#       a.reshape()
+#       a2.flatten()
+
+# Special array functions
+#   zeros()
+#   ones()
+#   full()
+#   eye()
+#   random.random()
+
+# Slicing
+#   Subarrays
+#   Slices and integers
+#   Indexing with integer arrays
+#   arange()
+
+# Boolean arrays
+#   Indexing with boolean arrays

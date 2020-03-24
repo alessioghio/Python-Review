b0d519052ceafb8a401317289492523742ffe91b
Alessio <alessiogdanz@gmail.com>
2020-03-23 23:12:13 -0500

    First add through git

---
+++ basicPython.py
@@ -0,0 +1,43 @@
+# Print
+
+# Variables and constants
+
+# Type of variables
+# string
+# s.capitalize(), s.upper(), s.lower(), s.replace(a, b)
+
+# Operators
+
+# Functions and imports
+
+# Conditions
+# if, elif, else
+
+# Loops
+# for, while
+# in range()
+
+# Pointers
+
+# Lists
+# copy function
+# append, pop
+
+# Slicing
+
+# Enumerate and double iterators in for loops
+
+# Dictionaries
+# d.items()
+
+# Tuples
+
+# Sets
+# s.add(), s.remove()
+
+# Classes
+# Constructor
+#   def __init__(self, variable=0):
+#       self.value = variable
+# Methods
+ 

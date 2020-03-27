98555d32f218ed0f922c5d26c480640feaa21e76
Alessio <alessiogdanz@gmail.com>
2020-03-26 22:52:09 -0500

    Updated .gitignore and edited basicPython.py (half reviwed)

--- basicPython.py
+++ basicPython.py
@@ -1,14 +1,62 @@
-# Print
+from myFunctions import expo
+import myFunctions
 
-# Variables and constants
+# Print
+# print(-3)
+# print("hola vomo estas")
+
+# Variables
+a = 80
+nombre = "bruno"
+apellido = "belevan"
+#print(a)
+a = 10
+#print(a)
 
 # Type of variables
 #   string
 #       s.capitalize(), s.upper(), s.lower(), s.replace(a, b), s.split()
+# print(nombre.capitalize())
+# print(nombre.upper())
+# print(nombre.replace('o','e'))
+oracion = "bruno es mi amigo"
+# print(oracion.split())
+nombre2 = nombre.capitalize()
+# print(nombre2)
+
+a = 80 # int
+gravedad = 9.81 # float o double SON PARA NUMEROS DECIMALES
+boolean = True # 1 o 0
+nombre = "bruno" # string
+
+# print(type(gravedad))
+# print(type(nombre))
+# print(type(boolean))
 
 # Operators
+# sum: +
+# subtract: -
+# division: /
+# multiplication: *
+# power: ** IMPORTANTE
+
+#print(nombre + " " + apellido)
+#print("La gravedad es: " + str(gravedad) + " m/s^2")
+#print("La gravedad es:", gravedad, "m/s^2")
+#print("La gravedad es: %.3f m/s^2" % (gravedad))
+#print("La gravedad es %f y %s lo sabe" % (gravedad,nombre))
 
 # Functions and imports
+def sumar2nums(num1, num2):
+    res = num1 + num2
+    return res
+
+resultado = sumar2nums(5,3)
+
+exponencial = expo(2,3)
+print(exponencial)
+
+myFunctions.welcomeMSG()
 
 # Conditions
 #   if, elif, else
---
+++ myFunctions.py
@@ -0,0 +1,10 @@
+##############################
+#---- These are my functions
+##############################
+
+def expo(num1, num2):
+    res = num1**num2
+    return res
+
+def welcomeMSG(): # void
+    print("Bienvenidos amigos de YouTube")
